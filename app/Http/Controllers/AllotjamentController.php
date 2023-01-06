4afc285c89dcb302ec0113bb103ae86462091b80
Jaume Fullana <jfullana@cifpfbmoll.eu>
2023-01-06 19:18:31 +0100

    updated response key to upper case

--- app/Http/Controllers/AllotjamentController.php
+++ app/Http/Controllers/AllotjamentController.php
@@ -11,12 +11,12 @@ class AllotjamentController extends Controller
 {
     public function getAllotjaments(){
         $allotjmanets = Allotjament::all();
-        return response()->json(["status" => "Success","result" => $allotjmanets], 200);
+        return response()->json(["Status" => "Success","Result" => $allotjmanets], 200);
     }
 
     public function getAllotjament($id){
         $allotjmanet = Allotjament::findOrFail($id);
-        return response()->json(["status" => "Success","result" => $allotjmanet], 200);
+        return response()->json(["Status" => "Success","Result" => $allotjmanet], 200);
     }
 
     public function insertAllotjament(Request $request){
@@ -28,7 +28,7 @@ class AllotjamentController extends Controller
         $isValid = Validator::make($request->all(), $validator, $messages);
 
         if ($isValid->fails()){
-            return response()->json(["status" => "Error","result"=>$isValid->errors()], 400);
+            return response()->json(["Status" => "Error","Result"=>$isValid->errors()], 400);
         }
 
         $allotjament->NumeroRegistre = $request->NumeroRegistre;
@@ -45,15 +45,15 @@ class AllotjamentController extends Controller
         $allotjament->TipusAllotjamentsID = $request->TipusAllotjamentsID;
 
         if ($allotjament->save()) {
-            return response()->json(['status' => 'Success','result' => $allotjament], 200);
+            return response()->json(['Status' => 'Success','Result' => $allotjament], 200);
         } else {
-            return response()->json(['status' => 'Error','result' => 'Error guardant'], 400);
+            return response()->json(['Status' => 'Error','Result' => 'Error guardant'], 400);
         }
     }
 
     public function updateAllotjament(Request $request){
         if ($request->ID == null || $request->ID < 0) {
-            return response()->json(["status" => "Error","result"=>"Incorrect ID"], 400);
+            return response()->json(["Status" => "Error","Result"=>"Incorrect ID"], 400);
         }
 
         $allotjament=Allotjament::findOrFail($request->ID);
@@ -63,7 +63,7 @@ class AllotjamentController extends Controller
         $isValid = Validator::make($request->all(), $validator, $messages);
 
         if ($isValid->fails()){
-            return response()->json(["status" => "Error","result"=>$isValid->errors()], 400);
+            return response()->json(["Status" => "Error","Result"=>$isValid->errors()], 400);
         }
 
         $allotjament->NumeroRegistre = $request->NumeroRegistre;
@@ -82,23 +82,23 @@ class AllotjamentController extends Controller
         $allotjament->TipusCategoriesID = $request->TipusCategoriesID;
 
         if ($allotjament->save()) {
-            return response()->json(['status' => 'Success','result' => $allotjament], 200);
+            return response()->json(['Status' => 'Success','Result' => $allotjament], 200);
         } else {
-            return response()->json(['status' => 'Error','result' => 'Error actualitzant'], 400);
+            return response()->json(['Status' => 'Error','Result' => 'Error actualitzant'], 400);
         }
     }
 
     public function deleteAllotjament(Request $request){
         if ($request->ID == null || $request->ID < 0) {
-            return response()->json(["status" => "Error","result"=>"Incorrect ID"], 400);
+            return response()->json(["Status" => "Error","Result"=>"Incorrect ID"], 400);
         }
 
         $allotjament=Allotjament::findOrFail($request->ID);
 
         if ($isDeleted = $allotjament->delete()) {
-            return response()->json(['status' => 'Success','result' => $isDeleted], 200);
+            return response()->json(['Status' => 'Success','Result' => $isDeleted], 200);
         } else {
-            return response()->json(['status' => 'Error','result' => 'Error borrant'], 400);
+            return response()->json(['Status' => 'Error','Result' => 'Error borrant'], 400);
         }
     }
 
