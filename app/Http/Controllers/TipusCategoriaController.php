fd9f1f83becd15e61233c4cf8bb7c81a451a94a7
Bielseguig <bielsegui@paucasesnovescifp.cat>
2023-02-23 20:56:50 +0100

    AcabarTipusCategoria

--- app/Http/Controllers/TipusCategoriaController.php
+++ app/Http/Controllers/TipusCategoriaController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Helpers\ControllersHelper;
 use App\Models\TipusCategoria;
+use App\Models\TipusServei;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TipusCategoriaController extends Controller
 {
@@ -65,5 +69,80 @@ class TipusCategoriaController extends Controller
     public function getTipusCategoria($id){
         $tipusServei = TipusCategoria::findOrFail($id);
         return response()->json(["Status" => "Success","Result" => $tipusServei], 200);
+    }
+
+        /**
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @OA\Post(
+     *    path="/api/tarifa",
+     *    tags={"Tipus Categoria"},
+     *    summary="Crea un tipus de categoria",
+     *    description="Crea un tipus de categoria.",
+     *    security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *        required=true,
+     *        @OA\JsonContent(
+     *           @OA\Property(property="ID", type="number", format="number", example="4"),
+     *           @OA\Property(property="Non", type="string", format="string", example="Basura"),
+     *
+     *        ),
+     *     ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="success"),
+     *         @OA\Property(property="data",type="object")
+     *          ),
+     *       ),
+     *    @OA\Response(
+     *         response=400,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="status", type="integer", example="error"),
+     *         @OA\Property(property="data",type="string", example="Atribut categoria requerit")
+     *          ),
+     *       )
+     *  )
+     */
+    public function insertTarifa(Request $request){
+
+        if ($request->DadesUsuari->RolsID != 3) {
+            return response()->json(["Status" => "Error", "Result" => "Privilegis insuficients."], 401);
+        }
+
+        $tipusServei = new TipusServei();
+        $idTipusServei = TipusServei::find($request->ID);
+
+        if ($request->ID == null || $request->ID < 1 || $request->ID == $idTipusServei->ID) {
+            return response()->json(["Status" => "Error","Result"=>"Incorrect ID"], 400);
+        }
+
+        $validator = $this->createValidator();
+        $messages = ControllersHelper::createValidatorMessages();
+
+        $isValid = Validator::make($request->all(), $validator, $messages);
+
+        if ($isValid->fails()){
+            return response()->json(["Status" => "Error","Result"=>$isValid->errors()], 400);
+        }
+
+        $tipusServei->ID = $request->ID;
+        $tipusServei->Nom = $request->Nom;
+
+        if ($tipusServei->save()) {
+            return response()->json(['Status' => 'Success','Result' => $tipusServei], 200);
+        } else {
+            return response()->json(['Status' => 'Error','Result' => 'Error guardant'], 400);
+        }
+    }
+
+    public function createValidator(): array
+    {
+        return [
+            "ID" => ["required"],
+            "Nom" => ["required"]];
     }
 }
