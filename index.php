522839c3dbac105ffb33d259c65eaf74b322b75c
Manel Josep Martinez Sanz <maneljosepmartinez@paucasesnovescifp.cat>
2023-03-03 19:52:29 +0100

    restaurarIndex

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-// header('Location: ' . "http://www.allotjamentsol.com/public/api/documentation", true, 303);
+header('Location: ' . "http://www.allotjamentsol.com/public/api/documentation", true, 303);
 die();
-
-?>
